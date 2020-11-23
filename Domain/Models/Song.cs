0fdf1b83a40e31d3be9bcb24fc84474065f923e6
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-11-23 17:01:21 +0300

    Initial commit

--- Domain/Models/Song.cs
+++ Domain/Models/Song.cs
@@ -1,7 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Models
 {
     public class Song
     {
-        
+        [Required] public int Id { get; set; }
+
+        [Required] public string Name { get; set; }
+
+        [Required] public string Artist { get; set; }
+
+        [Required] public string Album { get; set; }
+
+        [Required] public string Path { get; set; }
+
+        [Required] public string Tags { get; set; }
     }
 }
