76599368ee3f2277c2e38c836328c7281d37af10
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-11-25 23:23:00 +0300

    Added db

--- Repository/ApplicationDbContext.cs
+++ Repository/ApplicationDbContext.cs
@@ -10,5 +10,25 @@ namespace Repository
         public DbSet<EFAlbum> Albums { get; set; }
         public DbSet<EFTag> Tags { get; set; }
         public DbSet<EFUser> Users { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EFSong>()
+                .HasOne(s => s.Album)
+                .WithMany(a => a.Songs)
+                .HasForeignKey(s => s.AlbumId);
+
+            modelBuilder.Entity<EFSong>()
+                .HasMany(s => s.Tags)
+                .WithMany(t => t.Songs)
+                .UsingEntity(b => b.ToTable("AddedTags"));
+            
+            modelBuilder.Entity<EFAlbum>()
+                .HasOne(al => al.Artist)
+                .WithMany(ar => ar.Albums)
+                .HasForeignKey(al => al.ArtistId);
+        }
     }
 }
---
+++ Web/Services/Config.cs
@@ -0,0 +1,7 @@
+namespace Web.Services
+{
+    public class Config
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
